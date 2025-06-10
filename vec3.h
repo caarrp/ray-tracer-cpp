9eb5ca7979826d2325fbb0c7190843e49b86531e
caarrp <junebirdsfly@gmail.com>
2025-06-09 18:20:38 -0700

    functionality for vec3 h file

--- vec3.h
+++ vec3.h
@@ -1,9 +1,10 @@
 #include <math.h>
-#include <stdlib.>
+#include <stdlib.h>
 #include <iostream.h>
 
 #ifndef VEC3.H
 #define VEC3.H
+
 
 class vec3 {
 
@@ -14,16 +15,148 @@ public:
 
     vec3(double x, double y, double z)
     {x = x; y = y; z = z;}
-    inline double x() const { return x; }
-    inline double y() const { return y; }
-    inline double z() const { return z; }
-    inline double r() const { return x; }
-    inline double g() const { return y; }
-    inline double b() const { return z; }
+    
+    double x() const { return e[0]; }
+    
+    double y() const { return e[1]; }
+    
+    double z() const { return [2]; }
 
-    inline const vec& operator+() const { return *this; } 
-    inline vec operator-() const { return vec(-x, -y, -z); }
-    inline double operator[](int i) const {return 
+    //const vec& operator+() const { return *this; } 
+    vec operator-() const { 
+	return vec(-x, -y, -z); 
+	}
+    
+    double operator[](int i) const {
+	return e[i]
+	}
 
+    double operator[](int i) {
+	return e[i];
+	}
 
+    vec3& operator+=(const vec3& v){
+	//like a shifting operation
+	e[0] += v.e[0];
+	e[1] += v.e[1];
+	e[2] += v.e[2];
+	return *this;
+	}
+
+    vec3& operator*=(double s){
+	//like scalar operation
+	e[0] *= s;
+	e[1] *= s;
+	e[2] += s;
+	return *this;
+	}
+
+    vec& operator/=(double t){
+	//already using operator above wow
+	return *this *= 1/t;
+	}
+
+    double length() const{
+	return std::sqrt(length_squared());
+	}	
+
+    double length_squared(){
+	return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+	}
+
+    bool near_zero() const{
+	double n = 1e-8;
+	return (std::fabs(e[0] < n) && (std::fabs(e[1] < n) && (std::fabs(e[2] < n)
+	}
+    	
+    static vec3 random(){
+	return vec3(random_double(), random_double(), random_double());
+	}		
+
+    static vec3 random(low, high){
+	return vec3(random_double(low, high), random_double(low, high), random_double(low, high));
+	}
 };
+
+//vector utility functions
+
+inline vec3 operator+(const vec3 &u, const vec3 &v){
+    return vec3(u.e[0]+v.e[0], u.e[1]+v.e[1], u.e[2]+v.e[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v){
+    return vec3(u.e[0]-v.e[0], u.e[1]-v.e[1], u.e[2]-v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v){
+    return vec3(u.e[0]*v.e[0], u.e[1]*v.e[1], u.e[2]*v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, double s){
+    return vec3(u.e[0]*s, u.e[1]*s, u.e[2]*s);
+}
+
+inline vec3 operator*(double s, const vec3 &v){
+    return vec3(s*v.e[0], s*v.e[1], s*v.e[2]);
+}
+
+inline vec3 operator/(const vec3 &u, double t){
+    return u * (1/t);
+}
+
+inline double dot(const vec3 &u, const vec3 &v){
+    return (u.e[0]*v.e[0] + u.e[1]*v.e[1] + u.e[2]*v.e[2]);
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v){
+    return vec3(u.e[1]*v.e[2] - u.e[2]*v.e[1],
+	        u.e[0]*v.e[2] - u.e[0]*v.e[2],
+		u.e[0]*v.e[1] - u.e[1]*v.e[0]);
+}
+
+inline vec3 unit(const vec3 &u){
+    return u / u.length;
+}
+
+inline random_in_unit_disk() {
+    while (true) {
+	auto m = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+	if (m.length_squared() < 1){
+	    return m;
+	}
+    }
+}
+
+inline vec3 random_unit(){
+    while (true) {
+	auto m = vec3::random(-1, 1);
+	auto len_sq = m.length_squared();
+	if (1e-160 < len_sq && len_sq <= 1.0){
+	    return p / sqrt(len_sq);
+	}
+    }
+}
+
+inline vec3 random_on_hemisphere{
+	vec3 on_unit_sphere = random_unit();
+	if (dot(on_unit_sphere, normal) > 0.0){
+	    return on_unit_sphere;
+	    }
+	else{
+	    return -on_unit_sphere;
+	    }
+}
+
+inline vec3 reflect(const vec3 &u, const vec3 &n){
+    return u - 2*dot( u, n)*n;
+}
+inline vec3 refract(const vec3 &uv, const vec3 &n, double etai_over_etat){
+    auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+    vec3 r_tangent = etai_over_etat * (uv + cos_theta);
+    vec3 r_parallel = -std::sqrt(std::fabs(1.0 - r_tangent.length_squared()))*n;
+    return r_tangent + r_paralell;
+}
+
+#endif
+
+
