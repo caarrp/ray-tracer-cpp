70194757ca2763e599f7dda12e0b1e9b759d20a3
caarrp <junebirdsfly@gmail.com>
2025-07-07 11:21:10 -0700

    notes on curr color, will change soon

--- main.cpp
+++ main.cpp
@@ -6,14 +6,22 @@
 
 
 vec3 color(const ray &r, hitable *world){
+    //a very key part of the ray tracer, colors pixels
     hit_record rec;
+    //world conains all hitable objects
     if (world->hit(r, 0.0, 10000.0, rec)) {
+	//if the ray intersects with anything in range
 	return 0.5 * vec3(rec.normal.x()+1.0, rec.normal.y()+1.0, rec.normal.z()+1.0);
+	//return a color based off the surface normal
+	//maps to [-1,1] -> [0,1]
     }
+
     else{
+	//if the ray did not hit anything
 	vec3 unit_dir = unit(r.direction());
 	double t = 0.5 *(unit_dir.y() + 1.0);
 	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+	//return a linear interpolation between light blue and white
     }	
 }
 
@@ -33,7 +41,7 @@ int main(){
 
     hitable *list[2];
     list[0] = new sphere(vec3(0, 0, -1.0), 0.5);
-    list[1] = new sphere(vec3(0, -100.5, -1), 1000);
+    list[1] = new sphere(vec3(0, -100.5, -1.0), 100);
     hitable *world = new hitable_list(list, 2);
 
     for (int j = ny-1; j >= 0; j--){
