9a12b0a4483ddd40d72b0d8b87cafe861b862cbf
caarrp <junebirdsfly@gmail.com>
2025-07-07 10:07:57 -0700

    notes on hitable

--- hitable.h
+++ hitable.h
@@ -2,16 +2,23 @@
 #define HITABLE_H
 
 #include "ray.h"
+//include ray bc its needed for hit function
 
 struct hit_record {
+    //stores details of ray/obj intersection
     double t;
+    //distance along ray where hit occurs
     vec3 p;
+    //position in space where ray hits obj
     vec3 normal;
+    //important for shading/lighting calcs
 };
 
 class hitable {
+    //abstract base class of objs that can be "hit"
     public:
 	virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const = 0;
+	//virtual allows other class to override
 };
 
 #endif
