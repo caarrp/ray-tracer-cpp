7784aa4df76d830e302c74655d2b9a0c38dfbbed
caarrp <junebirdsfly@gmail.com>
2025-07-12 17:41:43 -0700

    testing camera commit

--- camera.h
+++ camera.h
@@ -1,3 +1,19 @@
 #ifndef "CAMERA_H"
 #define "CAMERA_H"
 
+
+
+/*double aspect_ratio = 16.0/9.0;
+ 
+ int image_width = 400;
+ 
+ image_height = int( image_width / aspect_ratio);
+ 
+ image_height = (image_height < 1) ? 1 : image_height;
+ //if the image height is less than one make it one, otherwise keep as is
+ 
+ double viewport_height = 2.0;
+ double viewport_width = viewport_height * (double(image_width)/image_height);
+
+ */
+
