fafcd06449423c0707dfd7c43fc6af48e6d28f7d
caarrp <junebirdsfly@gmail.com>
2025-07-16 16:46:49 -0700

    simple gamma functionality

--- color.h
+++ color.h
@@ -5,10 +5,22 @@
 #include "interval.h"
 
 
+inline double linear_to_gamma(double linear_component){
+    //simple linear to gamma function with adjustable gamma
+    if (linear_component > 0){
+	double gamma = 2.2;
+	return std::pow(linear_component, 1.0 / gamma);
+    }
+
+
 void write_color(std::ostream &out, const vec3 &pixel_color){
     double r = pixel_color.x();
     double g = pixel_color.y();
     double b = pixel_color.z();
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
     
     //translates [0,1] components to [0,255]
     static const interval intensity(0.000, 0.999);
